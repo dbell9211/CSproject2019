9186828d98d3f3a06bdcc6bc7748506767395699
dbell9211 <37660791+dbell9211@users.noreply.github.com>
2019-03-31 18:11:35 -0400

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,81 @@
+//import 
+//import
+//import 
+//import 
+//import 
+//import
+//import
+//import
+
+import java.io.*;
+import java.util.*; 
+
+public class Driver {
+   
+   //instance variables
+   static int cpu;
+   scheduler = new Scheduler();
+   dispatcher
+   register
+   //memory call
+   
+   
+   //call loader
+   
+   //call Scheduler
+   scheduler = new Scheduler();
+   this.Schedulerprocess = Schedulerprocess;
+   
+   public Driver() {//constructor
+
+   }
+   
+   loader() {
+   
+   }
+   
+   public boolean loadercheck() {
+      do {
+         
+      
+      }
+      while(!loader.isempty())
+   }
+   
+   public jobcomplete() {
+      //scheduler pseudocode here.
+      
+   
+   }
+   
+   public dispatcher() {
+   
+   }
+   
+   //interupt???
+      
+   public static main(/* put parameters from other classes here */) {
+   
+   }
+   
+   public static void Main(String []args) {
+   
+   int cpus = 1;
+   System.out.println("OS starting...")
+   //print all output here?
+   
+   //loader();
+   //while() {
+      //scheduler();
+      //dispatcher();
+      //CPU();
+      //waitforinterrupt();
+   }//for loop to implement what functions do what in which order.
+   
+   }// end main method
+}//end driver class
+   
+   
+   
+   
+      
